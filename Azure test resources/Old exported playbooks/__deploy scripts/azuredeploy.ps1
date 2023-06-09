e94cc622303caa1de0c9fc1c0e695840186ad2db
Jake Derkowski <45303650+JakeDerkowski96@users.noreply.github.com>
2023-06-08 23:56:08 -0500

    added more test resources

---
+++ Azure test resources/Old exported playbooks/__deploy scripts/azuredeploy.ps1
@@ -0,0 +1,37 @@
+param(
+    [Parameter(Mandatory = $true)]$ResourceGroup
+)
+
+# Connect to your Azure Active Directory
+Connect-AzureAD
+
+# check is RG exists, create one with the provided name if False
+Get-AzResourceGroup -Name $ResourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+if ($notPresent) {
+    Write-Host "This resource group does not exist. To create new resource group"
+    
+    $Location = Read-Host "Enter the location:"
+    
+    New-AzResourceGroup -Name $ResourceGroup `
+        -Location $Location `
+        # -Verbose
+}
+
+# Create unique deployment name
+$today = Get-Date -Format "MM-dd-yyyy"
+$suffix = Get-Random -Maximum 100
+$deploySuffix = $today + "_$suffix"
+
+# Prepare the deploy command's variables
+$Name = "Restrict-MDEUrl"
+$deploymentName = $Name + $deploySuffix
+$remoteUrl = "https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/Playbooks/$Name/alert-trigger/azuredeploy.json"
+$localTemplate = "Parameters/$Name.json"
+New-AzResourceGroupDeployment -Name $deploymentName `
+    -ResourceGroupName $ResourceGroup `
+    -TemplateUri $remoteUrl `
+    -TemplateParameterFile $localTemplate `
+    -Verbose
+
+
