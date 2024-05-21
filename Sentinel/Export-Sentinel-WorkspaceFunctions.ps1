64ccaf868acd6639e76d4ca3f3e555aa539ebfe9
Jake Derkowski <45303650+JakeDerkowski96@users.noreply.github.com>
2024-05-20 21:56:01 -0500

    Update Export-Sentinel-WorkspaceFunctions.ps1

--- Sentinel/Export-Sentinel-WorkspaceFunctions.ps1
+++ Sentinel/Export-Sentinel-WorkspaceFunctions.ps1
@@ -1,20 +1,21 @@
 param(
     [Parameter(Mandatory = $true)]$SubscriptionId,
     [Parameter(Mandatory = $true)]$ResourceGroup,
-    [Parameter(Mandatory = $true)]$WorkspaceName    
+    [Parameter(Mandatory = $true)]$WorkspaceName,
+    [Parameter()][string[]]$Categories
 )
 
 Connect-AzureAD
 
 # Only export saved queries from these categories
-$Categories = ("sec", "usage", "proxy", "win", "o365")
+# $Categories = ("sec", "usage", "proxy", "win", "o365")
 
 # $ExportedSearches = (Get-AzOperationalInsightsSavedSearch -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName).Value.Properties | Where-Object { $Categories -contains $_.Category }
 # Set the destination workspace
 
 $SavedQueries = (Get-AzOperationalInsightsSavedSearch -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName).Value.Properties 
 
-$destSubscriptionId = Read-Host 
+# $destSubscriptionId = Read-Host 
 
 
 
